7d88317cb530cfb4f65a166179d85caa62d001ec
Austin <aripang.com@gmail.com>
2021-08-31 22:40:49 +0800

    server initialized;

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+
+app.get('/ping', (req, res) => {
+  return res.send('Pong');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
