50800f7ace9e416b9195707cea019d42be167931
Austin <aripang.com@gmail.com>
2021-09-01 19:58:16 +0800

    arranged .env files;

--- front-end/src/app/components/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ValidatorComponent } from 'react-material-ui-form-validator';
-
-class InputValidator extends ValidatorComponent {
-  renderValidatorComponent() {
-
-  }
-}
